80fce8a821f29be14d1c8cf13f8e3e691e8ca101
Chase Grigsby <119604511+Chaseocity@users.noreply.github.com>
2022-12-05 21:06:20 -0600

    Added solution for day 2

---
+++ Day 2/D2P2.py
@@ -0,0 +1,54 @@
+#Advent of Code 2022
+#Day 2: Rock Paper Scissors
+#Part 2
+
+import os
+import sys
+
+#Declare variables
+score = 0
+totalScore = 0
+
+#Convert chars to choices
+rock = 'A'
+paper = 'B'
+scissors = 'C'
+lose = 'X'
+tie = 'Y'
+win = 'Z'
+
+#Open input file
+with open(os.path.join(sys.path[0], "d2input.txt"), 'r') as inputFile:
+    #Read lines from input file
+    for line in inputFile:
+        #Get choices from input line
+        them = line[0]
+        us = line[2]
+        #Calculate score from choices
+        if us == win and them == rock:
+            score = 8
+        if us == tie and them == rock:
+            score = 4
+        if us == lose and them == rock:
+            score = 3
+        if us == win and them == paper:
+            score = 9
+        if us == tie and them == paper:
+            score = 5
+        if us == lose and them == paper:
+            score = 1
+        if us == win and them == scissors:
+            score = 7
+        if us == tie and them == scissors:
+            score = 6
+        if us == lose and them == scissors:
+            score = 2
+
+        #Add score to total
+        totalScore = totalScore + score
+
+#Output max count
+print(totalScore)
+
+#Close input file
+inputFile.close()
